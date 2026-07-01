86798da75acd08af319249e1b94acbe62d86c2b0
Milad Heidarpour <mld.heidarpour@gmail.com>
2026-07-01 09:28:39 +0330

    Add project files.

---
+++ src/UIs/GeekNotes.UI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeekNotes.UI.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
